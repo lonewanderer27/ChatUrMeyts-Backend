d4e2c9aea0fbff96ef090c2a7a57259073a1d82c
lonewanderer27 <adrianejames27@gmail.com>
2024-06-23 22:28:49 +0800

    chore: Update project structure and dependencies

---
+++ src/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from .routers import hello
+import os
+import uvicorn
+
+chaturmates = FastAPI(
+    title="Chaturmates API",
+    description="API for Chaturmates",
+    version="0.1.0"
+)
+
+chaturmates.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+chaturmates.include_router(hello.router)
+
+
+@chaturmates.get("/")
+async def root():
+    return {"message": "Hello Klasmeyt!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(chaturmates, host="0.0.0.0", port=int(
+        os.environ.get("PORT", 2428)))
---
+++ src/routers/hello.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter
+
+router = APIRouter(
+    prefix="/hello",
+    tags=["Hello"],
+)
+
+
+@router.get("/", description="Say hello to the world")
+async def say_hello():
+    return {"message": "Hello World"}
+
+
+@router.get("/{name}", description="Say hello to a klasmeyt")
+async def say_hello(name: str):
+    return {"message": f"Hello klasmeyt {name}"}
