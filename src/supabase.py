9f4442eddc9478d7bf968614e00cd1894acfde16
lonewanderer27 <adrianejames27@gmail.com>
2024-11-03 22:41:42 +0800

    fix(client): add missing load_env

--- src/supabase.py
+++ src/supabase.py
@@ -3,6 +3,9 @@ import logging
 from dotenv import load_dotenv
 import os
 
+# Load environment variables from .env file
+load_dotenv()
+
 # Configure Logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
